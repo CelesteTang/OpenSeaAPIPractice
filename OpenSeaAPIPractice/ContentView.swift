387df833dc800e79ad68a4e8946eb94ba7faecea
CelesteTang <optimism2025@gmail.com>
2020-04-17 22:18:50 +0800

    chore: first create project

---
+++ OpenSeaAPIPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpenSeaAPIPractice
+//
+//  Created by 湯芯瑜 on 2020/4/17.
+//  Copyright © 2020 Hsin-Yu Tang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
